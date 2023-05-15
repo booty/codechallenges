8dc85ddf354b6530b60d84906b4d8c3560d58712
John Rose <johnedmundrose@gmail.com>
2023-05-15 18:29:49 -0400

    Failed solution

---
+++ add_two_numbers.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require "pry-byebug"
+require "benchmark/ips"
+
+BM_WARMUP_SECONDS = 0.5
+BM_TIME_SECONDS = 2
+DEBUG = true
+
+class ListNode
+  attr_accessor :val, :next
+
+  def to_s
+    result = []
+
+    current_node = self
+    loop do
+      result << "(#{current_node.val})"
+      break unless current_node.next
+
+      current_node = current_node.next
+    end
+
+    result.join("->")
+  end
+
+  def append(newval)
+    current_node = self
+
+    while current_node.next
+      current_node = current_node.next
+    end
+
+    puts "appending #{newval}"
+    current_node.next = ListNode.new(newval)
+  end
+
+  def initialize(val = 0, _next = nil)
+    @val = val
+    @next = _next
+  end
+end
+
+class Solutions
+  
+
+  def self.naive(l1, l2)
+  end
+end
+
+test_cases = [
+  { input: "abc", reps: 3, result: "cbacbacba" },
+  { input: "hello", reps: 2, result: "olleholleh" },
+  { input: "hellohellohello", reps: 1, result: "olleholleholleh" },
+]
+
+def putsif(str)
+  puts str if DEBUG
+end
+
+class String
+  def ellipsize(limit)
+    return self if length <= limit
+
+    "#{self[0..limit - 2]}…"
+  end
+end
+
+def benchmark_label(method_name, test_case)
+  "#{method_name}(#{test_case.except(:result).values.join(',').ellipsize(15)})"
+end
+
+Benchmark.ips do |bm|
+  bm.config(time: BM_TIME_SECONDS, warmup: BM_WARMUP_SECONDS)
+  methods = (Solutions.methods - Class.methods)
+  methods.each do |meth|
+    puts meth
+    test_cases.each_with_index do |tcase, _tindex|
+      print "  case #{tcase[:input]}"
+
+      work = lambda { Solutions.send(meth, tcase[:input], tcase[:reps]) }
+      actual_result = work.call
+
+      if actual_result == tcase[:result]
+        puts " ✅ pass (#{actual_result})"
+      else
+        puts " ❌ fail!"
+        puts "    actual: #{actual_result}"
+        puts "    expected: #{tcase[:result]}"
+      end
+
+      label = benchmark_label(meth, tcase)
+      bm.report(label) { work.call } unless DEBUG
+    end
+  end
+end
--- regular_expression_matching.rb
+++ regular_expression_matching.rb
@@ -46,53 +46,122 @@ def classify(c)
   end
 end
 
-class Solutions
-  def self.cheat(str, pat)
-    !!(str =~ /\A#{pat}\Z/)
+# returns the length of the match (can be zero)
+def match_zero_or_more(str, start_pos, match_char, stop_char)
+  pos = start_pos
+  stop_char_equals_first_char = (str[pos] == stop_char)
+  reps = 0
+
+  while (pos < str.length)
+    reps += 1
+    raise "too many reps" if reps > 25
+
+    c = str[pos]
+
+    putsif("   match_zero_or_more | pos:#{pos} c:#{c} match_char:#{match_char} stop_char:#{stop_char}")
+
+    unless c  # end of string
+      putsif("     end of string")
+      break
+    end
+    if (c == stop_char) && !stop_char_equals_first_char
+      putsif("     hit the stop char (#{c} == #{stop_char})")
+      break
+    end
+    unless (c == match_char) || (match_char==".")
+      putsif("     no match (#{c} != #{match_char})")
+      break
+    end
+    pos += 1
   end
 
-  def self.chunky(str, pat)
-    chunks = str.split("*")
+  result = pos - start_pos
+  if result.positive? && stop_char_equals_first_char
+    putsif("    back it up by one since stop_char_equals_first_char")
+    result -= 1
+  end
 
+  putsif("   match_zero_or_more returning #{result}")
+  result
+end
+
+# 01234567890
+# mississippi
+
+
+class Solutions
+  # def self.cheat(str, pat)
+  #   !!(str =~ /\A#{pat}\Z/)
+  # end
+
+  def self.chunky(str, pat)
+    # special simple case
+    unless pat.include?("*") || pat.include?(".")
+      return str == pat
+    end
+
+    putsif("....start....")
     str_pos = 0
     pat_pos = 0
+    result = true
 
-    while pat_pos < pat.length
+    while (pat_pos < pat.length) && result
       pat_current_c = pat[pat_pos]
       pat_next_c = pat[pat_pos + 1]
       pat_current_type = classify(pat_current_c)
       pat_next_type = classify(pat_next_c)
+      str_current_c = str[str_pos]
 
-      debug_string = "pat_pos:#{pat_pos} " \
-        "pat_current_c:#{pat_current_c}(#{pat_current_type}) " \
-        "pat_next_c:#{pat_next_c}(#{pat_next_type}) "
+      # debug_string = "pat_pos:#{pat_pos} " \
+      #   "pat_current_c:#{pat_current_c}(#{pat_current_type}) " \
+      #   "pat_next_c:#{pat_next_c}(#{pat_next_type}) "
+
+      debug_string = "str_pos:#{str_pos}(#{str_current_c}) " \
+        "pat_pos:#{pat_pos}(#{pat_current_c}, #{pat_current_type}) " \
+        "pat_next:(#{pat_next_c}, #{pat_next_type})"
 
       if pat_current_type == :zero_or_more
         raise "#{debug_string} unexpected #{pat_current_c}"
       end
 
+      if pat_next_c.nil? && (pat_current_c == ".")
+        putsif "#{debug_string} special case: "
+      end
+
       case [pat_current_type, pat_next_type]
       when [:literal, :literal], [:literal, :eos]
-        putsif "#{debug_string} should match literal #{pat_current_c}"
-      when [:literal, :zero_or_more]
-        putsif "#{debug_string} should match one or more #{pat_current_c}"
+        putsif "#{debug_string} should match literal #{str_current_c}==#{pat_current_c}?"
+        result = (pat_current_c == str_current_c) || (pat_current_c == ".")
+        str_pos += 1
         pat_pos += 1
-      # when [:one_or_more, :one_or_more]
-      #   raise "#{debug_string} unexpected double operator"
+      when [:literal, :zero_or_more]
+        putsif "#{debug_string} should match zero or more #{pat_current_c}"
+        str_pos += match_zero_or_more(str, str_pos, pat_current_c, pat[pat_pos + 2])
+        pat_pos += 2
       else
         raise "#{debug_string} i'm so confused"
       end
 
-      pat_pos += 1
     end
+
+    putsif("we're at the end str.length:#{str.length} str_pos:#{str_pos}")
+    return false unless str_pos == str.length
+
+    putsif("guess the result is... #{result}")
+    result
   end
 end
 
 test_cases = [
-  { input: "aa", pattern: "a", result: false },
-  { input: "john", pattern: "john", result: true },
-  { input: "aa", pattern: "a*", result: true },
-  { input: "ab", pattern: ".*", result: true },
+  # { input: "aaa", pattern: "a*a", result: true },
+  # { input: "john", pattern: "john", result: true },
+  # { input: "aa", pattern: "a", result: false },
+  # { input: "aa", pattern: "a*", result: true },
+  # { input: "ab", pattern: ".*", result: true },
+  # { input: "abc", pattern: ".*c", result: true },
+  { input: "abc", pattern: ".*d", result: false },
+  # { input: "mississippi", pattern: "mis*is*ip*.", result: true },
+  { input: "aaa", pattern: "ab*a*c*a", result: true },
 ]
 
 def putsif(str)
@@ -117,7 +186,7 @@ Benchmark.ips do |bm|
   methods.each do |meth|
     puts meth
     test_cases.each_with_index do |tcase, _tindex|
-      print "  case #{tcase[:input]}"
+      print "  case #{tcase[:input]} | #{tcase[:pattern]}"
 
       work = lambda { Solutions.send(meth, tcase[:input], tcase[:pattern]) }
       actual_result = work.call
