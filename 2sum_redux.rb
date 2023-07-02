317ac4d883e2fbbeefd9ada384e4928718b87159
John Rose <johnedmundrose@gmail.com>
2023-07-02 15:52:52 -0400

    baseline brute force

---
+++ 2sum_redux.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require_relative "testrunner"
+
+# https://leetcode.com/problems/two-sum/description/
+#
+# Given an array of integers nums and an integer target, return indices of
+# the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution, and you may
+# not use the same element twice.
+#
+# You can return the answer in any order.
+#
+# Example 1:
+#
+#   Input: nums = [2,7,11,15], target = 9
+#   Output: [0,1]
+#   Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#
+# Example 2:
+#
+#   Input: nums = [3,2,4], target = 6
+#   Output: [1,2]
+#
+# Example 3:
+#
+#   Input: nums = [3,3], target = 6
+#   Output: [0,1]
+#
+# Constraints:
+#
+#     2 <= nums.length <= 104
+#     -109 <= nums[i] <= 109
+#     -109 <= target <= 109
+#     Only one valid answer exists.
+#
+
+class Solutions
+  def self.brute(nums, target)
+    left = 0
+    len = nums.length
+    until left == len - 1
+      (left + 1).upto(len - 1) do |right|
+        putsif "left:#{left}, right:#{right} [#{nums[left]},#{nums[right]}]"
+        if nums[left] + nums[right] == target
+          putsif "  hooray!"
+          return [left, right]
+        end
+      end
+      left += 1
+    end
+  end
+end
+
+test_cases = [
+  {
+    params: [[3, 2, 4], 6],
+    result: [1, 2],
+  },
+  {
+    params: [[3, 3], 6],
+    result: [0, 1],
+  },
+  {
+    params: [[3, 2, 4], 6],
+    result: [1, 2],
+  },
+  {
+    params: [(1..1000).to_a, 1999],
+    result: [998, 999],
+    label: "1-10, sorted",
+    silent: true,
+  },
+]
+
+TestRunner.new.run(
+  solutions_klass: Solutions,
+  test_cases:,
+  # custom_comparison: ->(a, b) { a.to_set == b.to_set },
+)
