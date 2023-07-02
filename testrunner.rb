12eef2272da8dcf394c5f2f55c6be02478d0a8f0
John Rose <johnedmundrose@gmail.com>
2023-07-02 15:52:12 -0400

    cleanup, add silent option, respect ARGV

--- testrunner.rb
+++ testrunner.rb
@@ -1,8 +1,15 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/GlobalVars
+
 require "pry-byebug"
 require "benchmark/ips"
 
 BM_WARMUP_SECONDS = 0.5
 BM_TIME_SECONDS = 2
+
+$debug_only = ARGV.include? "--debugonly"
+$verbose = $debug_only
 
 class String
   def ellipsize(limit)
@@ -13,26 +20,48 @@ class String
 end
 
 def putsif(str)
-  puts str if DEBUG
+  puts str if $verbose
 end
 
 class TestRunner
   def benchmark_label(method_name, test_case)
-    "#{method_name}(#{test_case.except(:result).values.join(',').ellipsize(15)})"
+    test_case[:label] ||
+      "#{method_name}(#{test_case.except(:result).values.join(',').ellipsize(15)})"
+  end
+
+  def result(is_correct, actual_result, expected_result)
+    if is_correct
+      "\t✅ pass"
+    else
+      "\t❌ fail!" \
+        "\t\tactual: #{actual_result}" \
+        "\t\texpected: #{expected_result}"
+    end
+  end
+
+  def work_lambda(method_name, solutions_klass, tcase)
+    -> {
+      $verbose = false if tcase[:silent]
+      result = solutions_klass.send(method_name, *tcase[:params])
+      $verbose = $debug_only
+      result
+    }
   end
 
   def run(solutions_klass:, test_cases:, custom_comparison: nil)
     Benchmark.ips do |bm|
-      bm.config(time: BM_TIME_SECONDS, warmup: BM_WARMUP_SECONDS)
-      methods = (solutions_klass.methods - Class.methods)
-      methods.each do |method_name|
+      bm.config(
+        time: BM_TIME_SECONDS,
+        warmup: BM_WARMUP_SECONDS,
+      )
+
+      (solutions_klass.methods - Class.methods).each do |method_name|
         puts "method: #{method_name}"
         test_cases.each do |tcase|
-          puts "  case: #{tcase[:params]}"
+          label = benchmark_label(method_name, tcase)
+          puts "  case: #{label}"
 
-          work = lambda {
-            solutions_klass.send(method_name, *tcase[:params])
-          }
+          work = work_lambda(method_name, solutions_klass, tcase)
 
           actual_result = work.call
           expected_result = tcase[:result]
@@ -46,18 +75,13 @@ class TestRunner
                          actual_result == expected_result
                        end
 
-          if is_correct
-            puts "    ✅ pass"
-          else
-            puts "    ❌ fail!"
-            puts "       actual: #{actual_result}"
-            puts "       expected: #{expected_result}"
-          end
+          puts result(is_correct, actual_result, expected_result)
 
-          label = benchmark_label(method_name, tcase)
-          bm.report(label) { work.call } unless DEBUG
+          bm.report(label) { work.call } unless $debug_only
         end
       end
     end
   end
 end
+
+# rubocop:enable Style/GlobalVars
