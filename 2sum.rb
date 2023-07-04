a9443052e8b35561eaec5c9f8e80f3a35e9fc41a
John Rose <johnedmundrose@gmail.com>
2023-07-04 01:16:36 -0400

    Update to use new testrunner

--- 2sum.rb
+++ 2sum.rb
@@ -1,58 +1,50 @@
 # frozen_string_literal: true
 
-require "benchmark/ips"
+require_relative "testrunner"
 
 shuffled_array_1k = (1..1000).to_a.shuffle
 shuffled_array_10k = (1..10_000).to_a.shuffle
 shuffled_array_100k = (1..100_000).to_a.shuffle
 test_cases = [
   {
-    nums: [3, 2, 4],
-    target: 6,
+    params: [[3, 2, 4], 6],
     result: [1, 2],
-    name: "3 elements unsorted",
+    label: "3 elements unsorted",
   },
   {
-    nums: [30, 5, 100, 99, 3],
-    target: 33,
+    params: [[30, 5, 100, 99, 3], 33],
     result: [0, 4],
-    name: "5 elements unsorted",
+    label: "5 elements unsorted",
   },
   {
-    nums: [1, 2],
-    target: 3,
+    params: [[1, 2], 3],
     result: [0, 1],
-    name: "2 elements sorted",
+    label: "2 elements sorted",
   },
   {
-    nums: [1, 2, 3, 4, 5, 6],
-    target: 11,
+    params: [[1, 2, 3, 4, 5, 6], 11],
     result: [4, 5],
-    name: "6 elements sorted",
+    label: "6 elements sorted",
   },
   {
-    nums: (1..1000).to_a,
-    target: 1999,
+    params: [(1..1000).to_a, 1999],
     result: [998, 999],
-    name: "1000 elements sorted",
+    label: "1000 elements sorted",
   },
   {
-    nums: (1..1000).to_a.reverse,
-    target: 1999,
+    params: [(1..1000).to_a.reverse, 1999],
     result: [0, 1],
-    name: "1000 elements reverse sorted",
+    label: "1000 elements reverse sorted",
   },
   {
-    nums: shuffled_array_1k,
-    target: 3,
+    params: [shuffled_array_1k,3],
     result: [shuffled_array_1k.index(1), shuffled_array_1k.index(2)],
-    name: "1000 elements shuffled",
+    label: "1000 elements shuffled",
   },
   {
-    nums: shuffled_array_10k,
-    target: 3,
+    params: [shuffled_array_10k, 3],
     result: [shuffled_array_10k.index(1), shuffled_array_10k.index(2)],
-    name: "10K elements shuffled",
+    label: "10K elements shuffled",
   },
 ]
 
@@ -88,30 +80,7 @@ class Solutions
   end
 end
 
-(Solutions.methods - Class.methods).each do |meth|
-  puts meth
-  test_cases.each_with_index do |tcase, tindex|
-    puts "  case #{tindex} (#{tcase[:name]})"
-    actual_result = Solutions.send(meth, tcase[:nums], tcase[:target])
-    if actual_result.sort == tcase[:result].sort
-      puts "    ✅ pass"
-    else
-      puts "    ❌ fail!"
-      puts "    actual: #{actual_result}"
-      puts "    expected: #{tcase[:result]}"
-    end
-  end
-end
-
-Benchmark.ips do |x|
-  x.config(time: 1, warmup: 0.5)
-  (Solutions.methods - Class.methods).each do |meth|
-    test_cases.each_with_index do |tcase, tindex|
-      actual_result = nil
-      x.report("#{meth}: case #{tindex} (#{tcase[:name]})") do
-        # puts "  case #{tindex}"
-        actual_result = Solutions.send(meth, tcase[:nums], tcase[:target])
-      end
-    end
-  end
-end
+TestRunner.new.run(
+  solutions_klass: Solutions,
+  test_cases:,
+)
