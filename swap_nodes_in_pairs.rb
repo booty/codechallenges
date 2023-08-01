bf04b75d33b002423e2ba461c5fff2517c1b6153
John Rose <johnedmundrose@gmail.com>
2023-08-01 18:52:13 -0400

    works, performs

--- swap_nodes_in_pairs.rb
+++ swap_nodes_in_pairs.rb
@@ -32,7 +32,59 @@ class Solutions
   def self.letsgo(head)
     return head unless head # empty list
     return head unless head.next # single-element list
+
+    new_head = nil
+    lagger = nil
+    middle = head
+    leader = head.next
+
+    # suppose A->B->C->D
+    # initially: middle=A, leader=B
+    while leader
+      # lagger.next would now be A (if it existed)
+      lagger.next = leader if lagger
+
+      # A.next is now C
+      middle.next = leader.next
+
+      # B.next is now A
+      leader.next = middle
+
+      # list is now B->A->C->D
+      # lagger is now B
+      lagger = middle
+
+      # middle is now
+      middle = lagger.next
+
+      # middle is now B
+      new_head ||= leader
+      leader = lagger.next&.next
+
+      # putsif "lagger:#{lagger} " \
+      #        "middle:#{middle} " \
+      #        "leader:#{leader} " \
+      #        "new_head:#{new_head}"
+    end
+
+    new_head || head
   end
+end
+
+def input_array(num:)
+  (1..num).to_a
+end
+
+def result_array(num:)
+  input_array = input_array(num:)
+  (0..input_array.length - 1).step(2) do |i|
+    t = input_array[i + 1]
+    break unless t
+
+    input_array[i + 1] = input_array[i]
+    input_array[i] = t
+  end
+  input_array
 end
 
 test_cases = [
@@ -41,12 +93,26 @@ test_cases = [
     result: [2, 1, 4, 3],
   },
   {
+    params: [ListNode.from_array([1, 2, 3, 4, 5])],
+    result: [2, 1, 4, 3, 5],
+  },
+  {
     params: [ListNode.from_array],
     result: [],
   },
   {
     params: [ListNode.from_array([1])],
     result: [1],
+  },
+  {
+    params: [ListNode.from_array(input_array(num: 10_000))],
+    result: result_array(num: 10_000),
+    label: "10K elements",
+  },
+  {
+    params: [ListNode.from_array(input_array(num: 100_000))],
+    result: result_array(num: 100_000),
+    label: "100K elements",
   },
 ]
 
